752564895dddec2df48a847e0b0962b0d6bae071
JP Slavinsky <jpslav@gmail.com>
2020-07-13 09:37:08 -0600

    add profiling patch option

--- lib/kitchen/patches/nokogiri.rb
+++ lib/kitchen/patches/nokogiri.rb
@@ -84,5 +84,24 @@ module Nokogiri
       end
       # rubocop:enable Metrics/CyclomaticComplexity
     end
+
+    SEARCH_COUNTS = Hash.new(0)
+
+    if ENV['PROFILE']
+      # Patches inside Nokogiri to count and print searches.  At end of baking
+      # you can `puts Nokogiri::XML::SEARCH_COUNTS` to see the totals.  The counts
+      # hash is defined outside of the if block so that code that prints it doesn't
+      # explode if run without the env var.
+
+      class XPathContext
+        alias_method :original_evaluate, :evaluate
+        def evaluate(search_path, handler = nil)
+          SEARCH_COUNTS[search_path] += 1
+          puts search_path
+          original_evaluate(search_path, handler)
+        end
+      end
+    end
+
   end
 end
