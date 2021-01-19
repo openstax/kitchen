37f282135a491a51943165d032825f42d857ea31
brittany-johnson <brittanyj.email@gmail.com>
2021-01-19 16:13:17 -0600

    start spec for ancestor

---
+++ spec/ancestor_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+RSpec.describe Kitchen::Ancestor do
+  it "wraps elements that are ancestors of other elements and tracks how many decendants an ancestor has and their types"
+
+  describe '#initalize' do
+    it 'creates a new ancestor' do
+      #expect()
+    end
+  end
+
+  describe '#increment_descendant_count' do
+    it 'adds 1 to the descendant count for the given type' do
+      #expect()
+    end
+  end
+
+  describe '#decrement_descendant_count' do
+    it 'decreases the descendant count for the given type by some amount' do
+      #expect()
+    end
+  end
+
+  describe '#get_descendant_count' do
+    it 'returns the descendant count for the given type' do
+      #expect()
+    end
+  end
+
+  describe '#clone' do
+    it 'makes a new ancestor around the same element with new counts' do
+      #expect()
+    end
+  end
+end
