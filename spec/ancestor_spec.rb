f85d96e433ef11e204675b2efe75adc7d240ff58
brittany-johnson <brittanyj.email@gmail.com>
2021-01-21 10:23:47 -0600

    add openstruct for page element, more on init spec

--- spec/ancestor_spec.rb
+++ spec/ancestor_spec.rb
@@ -1,35 +1,44 @@
 require 'spec_helper'
+require 'ostruct'
 
 RSpec.describe Kitchen::Ancestor do
-  it "wraps elements that are ancestors of other elements and tracks how many decendants an ancestor has and their types"
+
+  let(:element) { OpenStruct.new(short_type: 'page') }
+  let(:ancestor) { described_class.new(element) }
 
   describe '#initalize' do
-    it 'creates a new ancestor' do
-      #expect()
+    it 'stores the element' do
+      expect(ancestor.element).to eq element
+    end
+    it 'stores the element\'s type' do
+      expect(ancestor.type).to eq element.short_type
+    end
+    it 'initalizes empty hash @descendant_counts' do
+      expect(ancestor.descendant_counts).to eq {}
     end
   end
 
   describe '#increment_descendant_count' do
     it 'adds 1 to the descendant count for the given type' do
-      #expect()
+      # expect()
     end
   end
 
   describe '#decrement_descendant_count' do
     it 'decreases the descendant count for the given type by some amount' do
-      #expect()
+      # expect()
     end
   end
 
   describe '#get_descendant_count' do
     it 'returns the descendant count for the given type' do
-      #expect()
+      # expect()
     end
   end
 
   describe '#clone' do
     it 'makes a new ancestor around the same element with new counts' do
-      #expect()
+      # expect()
     end
   end
 end
