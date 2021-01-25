83b87a346d660945335e316f9e47c2824c5f4103
Alan Cooke <12678235+Stackblocks@users.noreply.github.com>
2021-01-25 09:03:11 -0600

    Group tests requiring dummy data into a context
    In order to DRY out code and not repeat the "add one item to pantry"
    line four times I placed them into a context block with a `before`
    so the line only appears once.
    
    Re: #48

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -7,28 +7,29 @@ RSpec.describe Kitchen::Pantry do
     expect(instance).to be_truthy
   end
 
-  it 'adds an item to the pantry with the provided label' do
-    instance.store 'bar', label: 'foo'
-    expect(instance).to include(:foo)
-  end
+  context 'When the pantry has contents' do
+    before { instance.store 'bar', label: 'foo' }
 
-  it 'gets an item from the pantry' do
-    instance.store 'bar', label: 'foo'
-    expect(instance.get(:foo)).to eq('bar')
-  end
+    it 'adds an item to the pantry with the provided label' do
+      expect(instance).to include(:foo)
+    end
 
-  it 'raises if item is not in pantry' do
-    expect{ instance.get!('thud') }.to raise_error(
-      Kitchen::RecipeError, "There is no pantry item labeled 'thud'"
-    )
-  end
+    it 'gets an item from the pantry' do
+      expect(instance.get(:foo)).to eq('bar')
+    end
 
-  it 'iterate over the pantry items' do
-    expect(instance.each(&block)).to include(TK)
-  end
+    it 'raises if requested item is not in pantry' do
+      expect{ instance.get!('thud') }.to raise_error(
+        Kitchen::RecipeError, "There is no pantry item labeled 'thud'"
+      )
+    end
 
-  it 'returns the number of items in the pantry' do
-    instance.store 'bar', label: 'foo'
-    expect(instance.size).to eq(1)
+    it 'iterates over the pantry items' do
+      expect(instance.each(&block)).to include(TK)
+    end
+
+    it 'returns the number of items in the pantry' do
+      expect(instance.size).to eq(1)
+    end
   end
 end
