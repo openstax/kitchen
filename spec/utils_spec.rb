21baba9fb530e4cbde33608735036e85d89c1955
Mary Jo <mj81000@gmail.com>
2021-01-26 15:37:31 -0600

    fix test descriptor

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Kitchen::Utils do
-  describe '.search_path_to_type(search_path' do
+  describe '.search_path_to_type' do
     it 'converts a search path to an element type' do
       blt = ['bread', ['bacon', 'lettuce & tomato']]
       expect(described_class.search_path_to_type(blt)).to eq('bread,bacon,lettuce & tomato')
