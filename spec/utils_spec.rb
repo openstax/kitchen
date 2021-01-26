31d9d0676e5d7759f8f67d3ac8fd988430b26070
Mary Jo <mj81000@gmail.com>
2021-01-26 10:21:03 -0600

    adds Utils test using a BLT

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
-RSpec.describe Utils do
+RSpec.describe Kitchen::Utils do
   describe '.search_path_to_type(search_path' do
-    it "converts a search path to an element type" do
-      expect(described_class.search_path_to_type(["lol", "wow"])).to eq("lol wow")
+    it 'converts a search path to an element type' do
+      blt = ['bread', ['bacon', 'lettuce & tomato']]
+      expect(described_class.search_path_to_type(blt)).to eq('bread,bacon,lettuce & tomato')
     end
   end
 end
