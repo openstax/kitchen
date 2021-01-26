801652b58757222c8caa94b2ce704ca907e37db4
Alan Cooke <12678235+Stackblocks@users.noreply.github.com>
2021-01-26 15:32:39 -0600

    Clears up describtive lines and beefs up tests
    - Added the tests JP mentioned in his review
    - Formatted the file to output clearer results
    - Resolve conflicts
    
    Re: #48

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -3,34 +3,50 @@ require 'spec_helper'
 RSpec.describe Kitchen::Pantry do
   let(:instance) { described_class.new }
 
-  it 'creates a hash' do
-    expect(instance).to be_truthy
+  describe 'initialize' do
+    it 'creates a pantry object' do
+      expect(instance).to be_truthy
+    end
   end
 
-  context 'when the pantry has contents' do
+  context 'when at least one item is in the pantry' do
     before { instance.store 'bar', label: 'foo' }
 
-    it 'adds an item to the pantry with the provided label' do
-      expect(instance).to include(:foo)
+    describe '#get' do
+      it 'returns a present item with either a string or symbol label' do
+        expect(instance.get(:foo)).to eq('bar')
+        expect(instance.get('foo')).to eq('bar')
+      end
+
+      it 'returns nil when an item is not present' do
+        expect(instance.get(:blah)).to be_nil
+      end
     end
 
-    it 'gets an item from the pantry' do
-      expect(instance.get(:foo)).to eq('bar')
+    describe '#get!' do
+      it 'returns a present item with either a string or symbol label' do
+        expect(instance.get!(:foo)).to eq('bar')
+        expect(instance.get!('foo')).to eq('bar')
+      end
+
+      it 'raises if requested item is not in pantry' do
+        expect { instance.get!('thud') }.to raise_error(
+          Kitchen::RecipeError, "There is no pantry item labeled 'thud'"
+        )
+      end
     end
 
-    it 'raises if requested item is not in pantry' do
-      expect{ instance.get!('thud') }.to raise_error(
-        Kitchen::RecipeError, "There is no pantry item labeled 'thud'"
-      )
+    describe '#size' do
+      it 'returns the number of items in the pantry' do
+        expect(instance.size).to eq(1)
+      end
     end
 
-    it 'iterates over the pantry items' do
-      instance.store 'baz', label: 'qux'
-      expect { |block| instance.each(&block) }.to yield_successive_args([:foo, 'bar'], [:qux, 'baz'])
-    end
-
-    it 'returns the number of items in the pantry' do
-      expect(instance.size).to eq(1)
+    describe '#each' do
+      it 'iterates over the pantry items' do
+        instance.store 'baz', label: 'qux'
+        expect { |block| instance.each(&block) }.to yield_successive_args([:foo, 'bar'], [:qux, 'baz'])
+      end
     end
   end
 end
