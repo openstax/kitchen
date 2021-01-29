7e349bcefaf913006670268b687a109e8d1bfa96
Karina Mendez <karinamendez@Karinas-MacBook-Pro.local>
2021-01-29 12:11:46 -0600

    draft spec

--- spec/directions/bake_math_in_paragraph_spec.rb
+++ spec/directions/bake_math_in_paragraph_spec.rb
@@ -1,11 +1,35 @@
 require 'spec_helper'
 
 RSpec.describe Kitchen::Directions::BakeMathInParagraph do
-
-  # TODO: test that...
-  # 1) math as a top child and inside a subelement both get wrapped
-
-  xit 'works' do
+  let(:book) do
+    book_containing(html:
+      <<~HTML
+        <div>
+          <p>
+            <math>
+              <msup>
+                <mn>10</mn>
+                <mn>3</mn>
+              </msup>
+            </math>
+          </p>
+        </div>
+      HTML
+    )
   end
 
+  it 'works' do
+    #this works
+    puts book.search('p math').count
+    #this doesnt
+    puts described_class.v1(book: book)
+    puts book.body
+
+    #expect(
+    #  described_class.v1(chapter: chapter, metadata_source: metadata_element)
+    #).to match_normalized_html(
+    #  <<~HTML
+    #  HTML
+    #)
+  end
 end
