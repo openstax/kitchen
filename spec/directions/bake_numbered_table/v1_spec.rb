bebbc627775a0f7c3b0fc26aa15167af9c17be64
Kendra Swanson <kendraswan@gmail.com>
2021-02-01 22:29:01 +0000

    Add spec to cover additional table type

--- spec/directions/bake_numbered_table/v1_spec.rb
+++ spec/directions/bake_numbered_table/v1_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Kitchen::Directions::BakeNumberedTable::V1 do
 
   let(:caption) { '<caption>A caption</caption>' }
 
-  let(:table) do
+  let(:top_titled_table) do
     book_containing(html:
       one_chapter_with_one_page_containing(
         <<~HTML
@@ -35,10 +35,35 @@ RSpec.describe Kitchen::Directions::BakeNumberedTable::V1 do
     ).tables.first
   end
 
-  it 'works' do
-    described_class.new.bake(table: table, number: '2.3')
+  let(:other_table) do
+    book_containing(html:
+      one_chapter_with_one_page_containing(
+        <<~HTML
+          <table class="some-class" id="tId" summary="Some summary">
+            #{caption}
+            <thead>
+              <tr>
+                <th>A title</th>
+              </tr>
+              <tr>
+                <th>Another heading cell</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>One lonely cell</td>
+              </tr>
+            </tbody>
+          </table>
+        HTML
+      )
+    ).tables.first
+  end
 
-    expect(table.document.search('.os-table').first).to match_normalized_html(
+  it 'bakes a top-titled table' do
+    described_class.new.bake(table: top_titled_table, number: '2.3')
+
+    expect(top_titled_table.document.search('.os-table').first).to match_normalized_html(
       <<~HTML
         <div class="os-table os-top-titled-container">
           <div class="os-table-title">A title</div>
@@ -66,12 +91,46 @@ RSpec.describe Kitchen::Directions::BakeNumberedTable::V1 do
     )
   end
 
+  it 'bakes another kind of table' do
+    described_class.new.bake(table: other_table, number: '2.3')
+
+    expect(other_table.document.search('.os-table').first).to match_normalized_html(
+      <<~HTML
+        <div class="os-table">
+          <table class="some-class" id="tId" summary="Table 2.3  A caption">
+            <thead>
+              <tr>
+                <th>A title</th>
+              </tr>
+              <tr>
+                <th>Another heading cell</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>One lonely cell</td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="os-caption-container">
+            <span class="os-title-label">Table </span>
+            <span class="os-number">2.3</span>
+            <span class="os-divider"> </span>
+            <span class="os-divider"> </span>
+            <span class="os-caption">A caption</span>
+          </div>
+        </div>
+      HTML
+    )
+  end
+
+
   context 'no caption' do
     let(:caption) { '' }
 
     it 'does not include an os-caption div' do
-      described_class.new.bake(table: table, number: '2.3')
-      expect(table.document).not_to match('os-caption')
+      described_class.new.bake(table: top_titled_table, number: '2.3')
+      expect(top_titled_table.document).not_to match('os-caption')
     end
   end
 
