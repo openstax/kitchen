57daad0e7e7fccc01fbee01b2a11c658e4043209
Kendra Swanson <kendraswan@gmail.com>
2021-02-05 21:14:10 +0000

    fix failing bake_preface spec

--- spec/directions/bake_preface/v1_spec.rb
+++ spec/directions/bake_preface/v1_spec.rb
@@ -17,22 +17,20 @@ RSpec.describe Kitchen::Directions::BakePreface::V1 do
   it 'works' do
     described_class.new.bake(book: book1)
 
-    expect(book1).to match_normalized_html(
+    expect(book1.body).to match_normalized_html(
       <<~HTML
-        <html xmlns="http://www.w3.org/1999/xhtml">
-          <body>
-            <div class="preface" data-type="page">
+        <body>
+          <div class="preface" data-type="page">
+            <h1 data-type="document-title">
+              <span class="os-text" data-type="" itemprop="">Preface</span>
+            </h1>
+            <div data-type="metadata">
               <h1 data-type="document-title">
                 <span class="os-text" data-type="" itemprop="">Preface</span>
               </h1>
-              <div data-type="metadata">
-                <h1 data-type="document-title">
-                  <span class="os-text" data-type="" itemprop="">Preface</span>
-                </h1>
-              </div>
             </div>
-          </body>
-        </html>
+          </div>
+        </body>
       HTML
     )
   end
