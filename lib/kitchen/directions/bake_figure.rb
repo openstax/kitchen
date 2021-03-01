1c426d76196da1813873eddf84cb4dd3894da820
Mary Jo <mj81000@gmail.com>
2021-03-01 10:40:12 -0600

    add v2 and internationalize

--- lib/kitchen/directions/bake_figure.rb
+++ lib/kitchen/directions/bake_figure.rb
@@ -4,13 +4,32 @@ module Kitchen
       def self.v1(figure:, number:)
         figure.wrap(%(<div class="os-figure#{' has-splash' if figure.has_class?('splash')}">))
 
-        figure.document.pantry(name: :link_text).store "Figure #{number}", label: figure.id
+        figure.document.pantry(name: :link_text).store "#{I18n.t(:figure)} #{number}", label: figure.id
 
         caption = figure.caption&.cut
         figure.append(sibling:
           <<~HTML
             <div class="os-caption-container">
-              <span class="os-title-label">Figure </span>
+              <span class="os-title-label">#{I18n.t(:figure)} </span>
+              <span class="os-number">#{number}</span>
+              <span class="os-divider"> </span>
+              <span class="os-divider"> </span>
+              #{'<span class="os-caption">' + caption.children.to_s + '</span>' if caption}
+            </div>
+          HTML
+        )
+      end
+
+      def self.v2(figure:, number:)
+        figure.wrap(%(<div class="os-figure#{' has-splash' if figure.has_class?('splash')}">))
+
+        figure.document.pantry(name: :link_text).store "#{I18n.t(:figure)} #{number}", label: figure.id
+
+        caption = figure.first("div[data-type='title']")&.cut
+        figure.append(sibling:
+          <<~HTML
+            <div class="os-caption-container">
+              <span class="os-title-label">#{I18n.t(:figure)} </span>
               <span class="os-number">#{number}</span>
               <span class="os-divider"> </span>
               <span class="os-divider"> </span>
