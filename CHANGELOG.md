faaa97076927c74c3963f1264e443299eb27da1f
KarinaMendez2 <13000365@galileo.edu>
2021-03-02 22:28:19 +0000

    requested changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.1.0] - 2021-03-02
+
+* Added a file for baking key concepts called (`bake_chapter_key_concepts`) and the respective specs.
+
 ## [2.0.0] - 2020-12-18
 
 * Changed the main gem source file to have the same name as the gem (`openstax_kitchen`) so that you don't have to `require` a different name than you use in your `gem` call.
--- spec/directions/bake_chapter_key_concepts_spec.rb
+++ spec/directions/bake_chapter_key_concepts_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Kitchen::Directions::BakeChapterKeyConcepts do
     chapter_element(
       page_element(
         <<~HTML
-          <div data-type="document-title">Page 1</div>
+          <h1 data-type="document-title">Page 1</h1>
           <section id="auto_7c_fs-id1" class="key-concepts">
             <h3 data-type="document-title">WWF History</h3>
             <p>Concepts blah.</p>
@@ -30,17 +30,7 @@ RSpec.describe Kitchen::Directions::BakeChapterKeyConcepts do
       <<~HTML
         <div data-type="chapter">
           <div data-type="page">
-            <div data-type="document-title">Page 1</div>
-            <section id="auto_7c_fs-id1" class="key-concepts">
-              <a href="#auto_7c_0">
-                <h3 data-type="document-title" id="auto_7c_0_copy_XXX">
-                  <span class="os-number">1.2</span>
-                  <span class="os-divider"> </span>
-                  <span class="os-text" data-type="" itemprop="">Page 1</span>
-                </h3>
-              </a>
-              <p>Concepts blah.</p>
-            </section>
+            <h1 data-type="document-title">Page 1</h1>
           </div>
           <div class="os-eoc os-key-concepts-container" data-type="composite-page" data-uuid-key=".key-concepts">
             <h2 data-type="document-title">
@@ -53,8 +43,8 @@ RSpec.describe Kitchen::Directions::BakeChapterKeyConcepts do
             <div class="os-key-concepts">
               <div class="os-section-area">
                 <section id="auto_7c_fs-id1" class="key-concepts">
-                  <a href="#auto_7c_0">
-                    <h3 data-type="document-title" id="auto_7c_0_copy_XXX">
+                  <a href="#auto_7c_">
+                    <h3 data-type="document-title" id="auto_7c_">
                       <span class="os-number">1.2</span>
                       <span class="os-divider"> </span>
                       <span class="os-text" data-type="" itemprop="">Page 1</span>
