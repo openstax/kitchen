a81404d24bf16e110c5ebab55bb569c661458411
Mary Jo <mj81000@gmail.com>
2021-03-04 10:50:16 -0600

    Adds methods to skip subfigures
    adds is_subfigure? and figure-specific count_as_descendant to figure_element.rb

--- lib/kitchen/figure_element.rb
+++ lib/kitchen/figure_element.rb
@@ -23,6 +23,24 @@ module Kitchen
       first('figcaption', "div[data-type='description']")
     end
 
+    # Returns true if the figure is a child of another figure
+    #
+    # @return [Boolean]
+    #
+
+    def is_subfigure?
+      parent.node.to_s.end_with?('</figure>')
+    end
+
+    # Increments the count of this element in all of this element's ancestors
+    # unless it's a subfigure
+
+    def count_as_descendant
+      @ancestors.each_pair do |type, ancestor|
+        @counts_in[type] = ancestor.increment_descendant_count(short_type) unless is_subfigure?
+      end
+    end
+
     # Returns true if this class represents the element for the given node
     #
     # @param node [Nokogiri::XML::Node] the underlying node
