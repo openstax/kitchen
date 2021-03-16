2bc37e223906fc977d220e4c79987eba55948c81
KarinaMendez2 <13000365@galileo.edu>
2021-03-16 22:36:38 +0000

    100 coverage

--- spec/directions/bake_exercises/main_spec.rb
+++ spec/directions/bake_exercises/main_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe Kitchen::Directions::BakeExercises do
     expect_any_instance_of(Kitchen::Directions::BakeExercises::EOB).to receive(:bake).with(book: 'blah', class_names: {})
     described_class.eob(book: 'blah', class_names: {})
   end
+
+  it 'calls bake example exercises' do
+    expect_any_instance_of(Kitchen::Directions::BakeExercises::InPlace).to receive(:example_exercises).with(exercise: 'blah')
+    described_class.example_exercises(exercise: 'blah')
+  end
+
+  it 'calls bake note exercises' do
+    expect_any_instance_of(Kitchen::Directions::BakeExercises::InPlace).to receive(:note_exercises).with(exercise: 'blah')
+    described_class.note_exercises(exercise: 'blah')
+  end
+
+  it 'calls bake section exercises' do
+    expect_any_instance_of(Kitchen::Directions::BakeExercises::InPlace).to receive(:section_exercises).with(exercise: 'blah', number: '123')
+    described_class.section_exercises(exercise: 'blah', number: '123')
+  end
 end
--- spec/directions/bake_notes_spec.rb
+++ spec/directions/bake_notes_spec.rb
@@ -41,6 +41,34 @@ RSpec.describe Kitchen::Directions::BakeNotes do
     )
   end
 
+  let(:book_with_checkpoints_and_theorems) do
+    book_containing(html:
+      one_chapter_with_one_page_containing(
+        <<~HTML
+          <div data-type="note" id="note_id" class="checkpoint">
+            <div data-type="exercise" id="exercise_id">
+              <div data-type="problem" id="problem_id">
+                <p>problem content</p>
+              </div>
+              <div data-type="solution" id="solution_id">
+                <p>solution content</p>
+              </div>
+              <div data-type="commentary" id="an_id" data-element-type="hint">
+                <div data-type="title" id="hint_title_id">Hint</div>
+                <p id="id"></p>
+              </div>
+            </div>
+          </div>
+
+          <div data-type="note" id="2note_id" class="theorem">
+            <div data-type="title" id="title2">Two Important Limits</div>
+            <p id="another_id">Content</p>
+          </div>
+        HTML
+      )
+    )
+  end
+
   before do
     stub_locales({
       'notes': {
@@ -60,7 +88,7 @@ RSpec.describe Kitchen::Directions::BakeNotes do
       it 'works' do
         described_class.v1(book: book)
 
-        expect(book.search("[data-type='note']").first).to match_normalized_html_with_stripping(
+        expect(book.notes.first).to match_normalized_html_with_stripping(
           <<~HTML
             <div data-type="note" id="noteId">
               <h3 class="os-title" data-type="title">
@@ -81,7 +109,7 @@ RSpec.describe Kitchen::Directions::BakeNotes do
       it 'works' do
         described_class.v1(book: book)
 
-        expect(book.search("[data-type='note']").first).to match_html_nodes(
+        expect(book.notes.first).to match_html_nodes(
           <<~HTML
             <div data-type="note" id="noteId" class="chemistry link-to-learning">
               <h3 class="os-title" data-type="title">
@@ -102,7 +130,7 @@ RSpec.describe Kitchen::Directions::BakeNotes do
       it 'works' do
         described_class.v1(book: book)
 
-        expect(book.search("[data-type='note']").first).to match_html_nodes(
+        expect(book.notes.first).to match_html_nodes(
           <<~HTML
             <div data-type="note" id="noteId" class="everyday-life">
               <h3 class="os-title" data-type="title">
@@ -121,5 +149,67 @@ RSpec.describe Kitchen::Directions::BakeNotes do
       end
     end
 
+    context 'with different classnames' do
+      let(:book) { book_with_checkpoints_and_theorems }
+
+      before do
+        described_class.v1(book: book)
+      end
+
+      it 'checkpoints' do
+        note = book.first('.checkpoint')
+        described_class.bake_checkpoint_note(note: note, number: 1.1)
+        expect(note).to match_normalized_html(
+          <<~HTML
+            <div data-type="note" id="note_id" class="checkpoint">
+              <h3 class="os-title">
+                <span class="os-title-label">Checkpoint </span>
+                <span class="os-number">1.1</span>
+                <span class="os-divider"> </span>
+              </h3>
+              <div class="os-note-body">
+                <div data-type="exercise" id="exercise_id" class="os-hasSolution unnumbered">
+                  <div data-type="problem" id="problem_id">
+                    <p>problem content</p>
+                  </div>
+                  <div data-type="solution" id="solution_id"><span class="os-divider"> </span>
+                    <a class="os-number" href="#exercise_id">1.1</a>
+                    <div class="os-solution-container ">
+                      <p>solution content</p>
+                    </div>
+                  </div>
+                  <div data-type="commentary" id="an_id" data-element-type="hint">
+                    <div data-type="title" id="hint_title_id">Hint</div>
+                    <p id="id"/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          HTML
+        )
+      end
+
+      it 'theorems' do
+        note = book.first('.theorem')
+        described_class.bake_theorems(note: note, number: 1.1)
+        expect(note).to match_normalized_html(
+          <<~HTML
+            <div data-type="note" id="2note_id" class="theorem" use-subtitle="true">
+              <div class="os-title">
+                <span class="os-title-label">Theorem </span>
+                <span class="os-number">1.1</span>
+                <span class="os-divider"> </span>
+              </div>
+              <div class="os-note-body">
+                <h4 class="os-subtitle" data-type="title" id="title2">
+                  <span class="os-subtitle-label">Two Important Limits</span>
+                </h4>
+                <p id="another_id">Content</p>
+              </div>
+            </div>
+          HTML
+        )
+      end
+    end
   end
 end
