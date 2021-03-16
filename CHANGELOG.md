aa3ff0a4154061f26c72b445b4db77a3864aba48
Mary Jo <mj81000@gmail.com>
2021-03-16 13:13:12 -0500

    Adds v2 table specs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+* Adds a NumberedTables v2
+
 ## [2.0.0] - 2020-12-18
 
 * Refactored bake_exercises to better support parallel work on multiple versions.
---
+++ spec/directions/bake_numbered_table/v2_spec.rb
@@ -0,0 +1,169 @@
+require 'spec_helper'
+
+RSpec.describe Kitchen::Directions::BakeNumberedTable::V2 do
+
+  before do
+    stub_locales({
+      'table_label': 'Table'
+    })
+  end
+
+  let(:caption_with_title) { '<caption>A caption <span data-type="title">Caption title</span></caption>' }
+
+  let(:top_titled_table) do
+    book_containing(html:
+      one_chapter_with_one_page_containing(
+        <<~HTML
+          <table class="top-titled" id="tId" summary="Some summary">
+            #{caption_with_title}
+            <thead>
+              <tr>
+                <th>A title</th>
+              </tr>
+              <tr>
+                <th>Another heading cell</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>One lonely cell</td>
+              </tr>
+            </tbody>
+          </table>
+        HTML
+      )
+    ).tables.first
+  end
+
+  let(:column_header_table) do
+    book_containing(html:
+      one_chapter_with_one_page_containing(
+        <<~HTML
+          <table class="column-header" id="tId" summary="column header summary">
+          </table>
+        HTML
+      )
+    ).tables.first
+  end
+
+  let(:other_table) do
+    book_containing(html:
+      one_chapter_with_one_page_containing(
+        <<~HTML
+          <table class="some-class" id="tId" summary="Some summary">
+            #{caption_with_title}
+            <thead>
+              <tr>
+                <th>A title</th>
+              </tr>
+              <tr>
+                <th>Another heading cell</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>One lonely cell</td>
+              </tr>
+            </tbody>
+          </table>
+        HTML
+      )
+    ).tables.first
+  end
+
+  it 'bakes a top-titled table' do
+    described_class.new.bake(table: top_titled_table, number: '2.3')
+
+    expect(top_titled_table.document.search('.os-table').first).to match_normalized_html(
+      <<~HTML
+        <div class="os-table os-top-titled-container">
+          <div class="os-table-title">A title</div>
+          <table class="top-titled" id="tId" summary="Table 2.3 Caption title A caption ">
+            <thead>
+              <tr>
+                <th>Another heading cell</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>One lonely cell</td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="os-caption-container">
+            <span class="os-title-label">Table </span>
+            <span class="os-number">2.3</span>
+            <span class="os-divider"> </span>
+            <span class="os-title" data-type="title">Caption title</span>
+            <span class="os-divider"> </span>
+            <span class="os-caption">A caption </span>
+          </div>
+        </div>
+      HTML
+    )
+  end
+
+  it 'bakes a column header table' do
+    described_class.new.bake(table: column_header_table, number: '2.3')
+
+    expect(column_header_table.document.search('.os-table').first).to match_normalized_html(
+      <<~HTML
+        <div class="os-table os-column-header-container">
+          <table class="column-header" id="tId" summary="Table 2.3 ">
+        </table>
+          <div class="os-caption-container">
+            <span class="os-title-label">Table </span>
+            <span class="os-number">2.3</span>
+            <span class="os-divider"> </span>
+            <span class="os-divider"> </span>
+            <span class="os-caption"></span>
+          </div>
+        </div>
+      HTML
+    )
+  end
+
+  it 'bakes another kind of table' do
+    described_class.new.bake(table: other_table, number: '2.3')
+
+    expect(other_table.document.search('.os-table').first).to match_normalized_html(
+      <<~HTML
+        <div class="os-table ">
+          <table class="some-class" id="tId" summary="Table 2.3 Caption title A caption ">
+            <thead>
+              <tr>
+                <th>A title</th>
+              </tr>
+              <tr>
+                <th>Another heading cell</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>One lonely cell</td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="os-caption-container">
+            <span class="os-title-label">Table </span>
+            <span class="os-number">2.3</span>
+            <span class="os-divider"> </span>
+            <span class="os-title" data-type="title">Caption title</span>
+            <span class="os-divider"> </span>
+            <span class="os-caption">A caption </span>
+          </div>
+        </div>
+      HTML
+    )
+  end
+
+  context 'when no caption title' do
+    let(:caption) { '' }
+
+    it 'does not include an os-title div' do
+      described_class.new.bake(table: top_titled_table, number: '2.3')
+      expect(top_titled_table.document).not_to match('os-title')
+    end
+  end
+
+end
