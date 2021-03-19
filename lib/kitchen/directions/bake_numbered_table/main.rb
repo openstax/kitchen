19a678f4fc78d7f5b64116375f46af744554b581
Mary Jo <mj81000@gmail.com>
2021-03-19 11:55:32 -0500

    Condenses into v1

--- lib/kitchen/directions/bake_numbered_table/main.rb
+++ lib/kitchen/directions/bake_numbered_table/main.rb
@@ -3,12 +3,8 @@
 module Kitchen
   module Directions
     module BakeNumberedTable
-      def self.v1(table:, number:)
-        V1.new.bake(table: table, number: number)
-      end
-
-      def self.v2(table:, number:)
-        V2.new.bake(table: table, number: number)
+      def self.v1(table:, number:, always_caption: false)
+        V1.new.bake(table: table, number: number, always_caption: always_caption)
       end
     end
   end
