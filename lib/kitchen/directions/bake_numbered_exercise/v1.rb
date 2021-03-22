a44bddd4c9eb93074f67f69f39fc5c2d8292ef0c
JP Slavinsky <jpslav@gmail.com>
2021-03-22 02:52:12 +0000

    move inplace code to BakeNumberedExercise

--- lib/kitchen/directions/bake_numbered_exercise/v1.rb
+++ lib/kitchen/directions/bake_numbered_exercise/v1.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
-module Kitchen::Directions::BakeExercises
-  class InPlace
-    def section_exercises(exercise:, number:)
+module Kitchen::Directions::BakeNumberedExercise
+  class V1
+    def bake(exercise:, number:)
       problem = exercise.problem
       solution = exercise.solution
 
-      problem_number = "<span class='os-number'>#{number}</span><span class='os-divider'>. </span>"
+      problem_number = "<span class='os-number'>#{number}</span>"
+
       if solution.present?
+        solution.id = "#{exercise.id}-solution"
         exercise.add_class('os-hasSolution')
-        problem_number = "<a class='os-number' href='##{exercise.id}-solution'>#{number}</a>
-          <span class='os-divider'>. </span>"
+        problem_number = "<a class='os-number' href='##{exercise.id}-solution'>#{number}</a>"
+
         solution.replace_children(with:
           <<~HTML
             <a class='os-number' href='##{exercise.id}'>#{number}</a>
             <span class='os-divider'>. </span>
-            <div class="os-solution-container ">#{solution.children}</div>
+            <div class="os-solution-container">#{solution.children}</div>
           HTML
         )
       end
@@ -23,6 +25,7 @@ module Kitchen::Directions::BakeExercises
       problem.replace_children(with:
         <<~HTML
           #{problem_number}
+          <span class='os-divider'>. </span>
           <div class="os-problem-container">#{problem.children}</div>
         HTML
       )
--- spec/directions/bake_exercises/main_spec.rb
+++ spec/directions/bake_exercises/main_spec.rb
@@ -8,24 +8,4 @@ RSpec.describe Kitchen::Directions::BakeExercises do
       .with(bake_eob: true, bake_section_title: true, book: 'blah', class_name: 'section.exercises')
     described_class.v1(book: 'blah')
   end
-
-  it 'calls bake eob exercises' do
-    expect_any_instance_of(Kitchen::Directions::BakeExercises::EOB).to receive(:bake).with(book: 'blah', class_names: {})
-    described_class.eob(book: 'blah', class_names: {})
-  end
-
-  it 'calls bake example exercises' do
-    expect_any_instance_of(Kitchen::Directions::BakeExercises::InPlace).to receive(:example_exercises).with(exercise: 'blah')
-    described_class.example_exercises(exercise: 'blah')
-  end
-
-  it 'calls bake note exercises' do
-    expect_any_instance_of(Kitchen::Directions::BakeExercises::InPlace).to receive(:note_exercises).with(exercise: 'blah')
-    described_class.note_exercises(exercise: 'blah')
-  end
-
-  it 'calls bake section exercises' do
-    expect_any_instance_of(Kitchen::Directions::BakeExercises::InPlace).to receive(:section_exercises).with(exercise: 'blah', number: '123')
-    described_class.section_exercises(exercise: 'blah', number: '123')
-  end
 end
