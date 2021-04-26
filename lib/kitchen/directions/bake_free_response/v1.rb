cb43938801bcd8a20eb579d253ac863e84d2fde2
Mary Jo <mj81000@gmail.com>
2021-04-26 15:48:43 -0500

    outline

---
+++ lib/kitchen/directions/bake_free_response/v1.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Kitchen::Directions::BakeFreeResponse
+  class V1
+    renderable
+
+    def bake(chapter:, metadata_source:, klass:, append_to: nil)
+      #bloop
+    end
+  end
+end
