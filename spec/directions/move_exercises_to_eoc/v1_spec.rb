24bbe612bebdd3c88a4d3248bc014199fce5cbab
wilkus27 <natalia.wilkus@openstax.pl>
2021-04-28 14:40:37 +0000

    add specs for v1 without append_to

--- spec/directions/move_exercises_to_eoc/v1_spec.rb
+++ spec/directions/move_exercises_to_eoc/v1_spec.rb
@@ -50,43 +50,81 @@ RSpec.describe Kitchen::Directions::MoveExercisesToEOC::V1 do
     )
   end
 
-  it 'works' do
-    described_class.new.bake(chapter: book_with_review_exercises.chapters.first, metadata_source: metadata_element, append_to: append_to, klass: 'review-exercises')
-    expect(append_to).to match_normalized_html(
-      <<~HTML
-        <div class="os-eoc os-chapter-review-container" data-type="composite-chapter" data-uuid-key=".chapter-review">
-          <h2 data-type="document-title" id="composite-chapter-1">
-            <span class="os-text">Chapter Review</span>
-          </h2>
-          <div data-type="metadata" style="display: none;">
-            <h1 data-type="document-title" itemprop="name">Chapter Review</h1>
-            <div>metadata</div>
-            </div>
-            <div class="os-eoc os-review-exercises-container" data-type="composite-page" data-uuid-key=".review-exercises">
-                <h3 data-type="document-title">
-                  <span class="os-text">foo</span>
-                </h3>
-                <div data-type="metadata" style="display: none;">
-                  <h1 data-type="document-title" itemprop="name">Review Exercises</h1>
-                  <div class="authors" id="authors_copy_1">Authors</div>
-                  <div class="publishers" id="publishers_copy_1">Publishers</div>
-                  <div class="print-style" id="print-style_copy_1">Print Style</div>
-                  <div class="permissions" id="permissions_copy_1">Permissions</div>
-                  <div data-type="subject" id="subject_copy_1">Subject</div>
-                </div>
-                <section id="sectionId1" class="review-exercises">
-                  <div data-type="exercise" id="exercise_id1">
-                      <div data-type="problem" id="problem_id1">
-                        <p>exercise content 1</p>
-                      </div>
-                      <div data-type="solution" id="solution_id1">
-                        <p>Solution content</p>
-                      </div>
+  context 'when append_to is not null' do
+    it 'works' do
+      described_class.new.bake(chapter: book_with_review_exercises.chapters.first, metadata_source: metadata_element, append_to: append_to, klass: 'review-exercises')
+      expect(append_to).to match_normalized_html(
+        <<~HTML
+          <div class="os-eoc os-chapter-review-container" data-type="composite-chapter" data-uuid-key=".chapter-review">
+            <h2 data-type="document-title" id="composite-chapter-1">
+              <span class="os-text">Chapter Review</span>
+            </h2>
+            <div data-type="metadata" style="display: none;">
+              <h1 data-type="document-title" itemprop="name">Chapter Review</h1>
+              <div>metadata</div>
+              </div>
+              <div class="os-eoc os-review-exercises-container" data-type="composite-page" data-uuid-key=".review-exercises">
+                  <h3 data-type="document-title">
+                    <span class="os-text">foo</span>
+                  </h3>
+                  <div data-type="metadata" style="display: none;">
+                    <h1 data-type="document-title" itemprop="name">Review Exercises</h1>
+                    <div class="authors" id="authors_copy_1">Authors</div>
+                    <div class="publishers" id="publishers_copy_1">Publishers</div>
+                    <div class="print-style" id="print-style_copy_1">Print Style</div>
+                    <div class="permissions" id="permissions_copy_1">Permissions</div>
+                    <div data-type="subject" id="subject_copy_1">Subject</div>
                   </div>
-                </section>
+                  <section id="sectionId1" class="review-exercises">
+                    <div data-type="exercise" id="exercise_id1">
+                        <div data-type="problem" id="problem_id1">
+                          <p>exercise content 1</p>
+                        </div>
+                        <div data-type="solution" id="solution_id1">
+                          <p>Solution content</p>
+                        </div>
+                    </div>
+                  </section>
+              </div>
+            </div>
+        HTML
+      )
+    end
+  end
+
+  context 'when append_to is null' do
+    it 'works' do
+      expect(
+        described_class.new.bake(chapter: book_with_review_exercises.chapters.first, metadata_source: metadata_element, klass: 'review-exercises')
+      ).to match_normalized_html(
+        <<~HTML
+          <div data-type="chapter">
+            <div class="os-eoc os-review-exercises-container" data-type="composite-page" data-uuid-key=".review-exercises">
+              <h2 data-type="document-title">
+                <span class="os-text">foo</span>
+              </h2>
+              <div data-type="metadata" style="display: none;">
+                <h1 data-type="document-title" itemprop="name">Review Exercises</h1>
+                <div class="authors" id="authors_copy_1">Authors</div>
+                <div class="publishers" id="publishers_copy_1">Publishers</div>
+                <div class="print-style" id="print-style_copy_1">Print Style</div>
+                <div class="permissions" id="permissions_copy_1">Permissions</div>
+                <div data-type="subject" id="subject_copy_1">Subject</div>
+              </div>
+              <section id="sectionId1" class="review-exercises">
+                <div data-type="exercise" id="exercise_id1">
+                    <div data-type="problem" id="problem_id1">
+                      <p>exercise content 1</p>
+                    </div>
+                    <div data-type="solution" id="solution_id1">
+                      <p>Solution content</p>
+                    </div>
+                </div>
+              </section>
             </div>
           </div>
-      HTML
-    )
+        HTML
+      )
+    end
   end
 end
