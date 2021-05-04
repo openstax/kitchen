f391a00e8bcebbf29a2c29745d7dd9da549244dd
Brittany Johnson <bj20@rice.edu>
2021-05-04 15:32:31 -0500

    clean up and add spec

--- lib/kitchen/directions/bake_references.rb
+++ lib/kitchen/directions/bake_references.rb
@@ -6,21 +6,29 @@ module Kitchen
     #
     module BakeReferences
       def self.v1(book:)
-        book.chapters.pages.references.each do |reference|
-          reference.prepend(child:
-            <<~HTML
-              <span class="os-reference-number">#{reference.count_in(:chapter)}</span>
-            HTML
-          )
-        end
-
-        chapter_references = book.chapters.pages.references.cut
+        metadata_elements = book.metadata.children_to_keep.copy
 
         book.chapters.each do |chapter|
+          chapter.search('[data-type="cite"]').each do |link|
+            link.prepend(child:
+              <<~HTML
+                <sup>#{link.count_in(:chapter)}</sup>
+              HTML
+            )
+          end
+          chapter.references.each do |reference|
+            reference.prepend(child:
+              <<~HTML.chomp
+                <span class="os-reference-number">#{reference.count_in(:chapter)}. </span>
+              HTML
+            )
+          end
+          chapter_references = chapter.pages.references.cut
+          chapter_title_no_num = chapter.title.search('.os-text')
           chapter.append(child:
             <<~HTML
               <div class="os-chapter-area">
-                #{chapter.title.text}
+                <h2 data-type="document-title">#{chapter_title_no_num}</h2>
                 #{chapter_references.paste}
               </div>
             HTML
@@ -35,6 +43,10 @@ module Kitchen
               <h1 data-type="document-title">
                 <span class="os-text">#{I18n.t(:references)}</span>
               </h1>
+              <div data-type="metadata" style="display: none;">
+                <h1 data-type="document-title" itemprop="name">#{I18n.t(:references)}</h1>
+                #{metadata_elements.paste}
+              </div>
               #{chapter_area_references.paste}
             </div>
           HTML
