443d1aeddd37d06f695a04435440d5426408abfc
Brittany Johnson <bj20@rice.edu>
2021-05-05 14:44:44 -0500

    clean up strat

--- lib/kitchen/directions/move_solutions_to_answer_key/strategies/american_government.rb
+++ lib/kitchen/directions/move_solutions_to_answer_key/strategies/american_government.rb
@@ -10,20 +10,8 @@ module Kitchen::Directions::MoveSolutionsToAnswerKey
       protected
 
       def bake_section(chapter:, append_to:, klass:)
-        section_solutions_set = []
-        chapter.search(".#{klass}").each do |section|
-          section.search('[data-type="solution"]').each do |solution|
-            section_solutions_set.push(solution.cut)
-          end
-        end
-
-        append_solution_area(section_solutions_set, append_to)
-      end
-
-      def append_solution_area(solutions, append_to)
-        solutions.each do |solution|
-          append_to.add_child(solution.raw)
-        end
+        section_solutions_set = chapter.search(".#{klass} [data-type='solution']").map(&:cut)
+        section_solutions_set.map { |solution| append_to.add_child(solution.raw) }
       end
     end
   end
