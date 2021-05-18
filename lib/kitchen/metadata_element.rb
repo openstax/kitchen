05c18777550731be76e55078f71323d0a8f19837
Mary Jo <mj81000@gmail.com>
2021-05-18 16:13:08 -0500

    remove unnecessary method

--- lib/kitchen/metadata_element.rb
+++ lib/kitchen/metadata_element.rb
@@ -30,14 +30,5 @@ module Kitchen
       search(%w([data-type='revised'] .authors .publishers .print-style .permissions
                 [data-type='subject']))
     end
-
-    # Returns true if this class represents the element for the given node
-    #
-    # @param node [Nokogiri::XML::Node] the underlying node
-    # @return [Boolean]
-    #
-    def self.is_the_element_class_for?(node, config:)
-      Selector.named(:metadata).matches?(node, config: config)
-    end
   end
 end
