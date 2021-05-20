975ff0d09da14e98a5e0eced14d78f1769c9d31a
Kendra Swanson <kendraswan@gmail.com>
2021-05-20 20:20:49 +0000

    match selector using regular expressions

--- lib/kitchen/patches/nokogiri.rb
+++ lib/kitchen/patches/nokogiri.rb
@@ -43,6 +43,38 @@ module Nokogiri
       def inspect
         to_s
       end
+
+      def quick_matches?(selector_str)
+        # selector is a string literal
+        selectors = selector_str.split(', ')
+        val = nil
+        selectors.each do |selector|
+          val = matcher_helper(selector)
+          return true if val
+        end
+        raise "Unrecognized selector #{selector_str}" if val.nil?
+
+        val
+      end
+
+      def matcher_helper(selector)
+        case selector
+        when /\A\.([\w-]+)\z/ # classes
+          classes.include?(selector[1..-1])
+        when /\A(\w+)\z/ # just a name
+          name == selector
+        when /\A([\w.]+)\z/ # name and one class
+          el_name, el_class = selector.split('.')
+          name == el_name && classes.include?(el_class)
+        when /(\w*)\[([\w-]+)=["|']([\w-]+)["|']\]/ # name[attribute="value"] or [attribute="value"]
+          _, el_name, attribute, value = selector.match(/(\w*)\[([\w-]+)=["|']([\w-]+)["|']\]/).to_a
+          self[:"#{attribute}"] == value && (el_name.empty? ? true : name == el_name)
+        end
+      end
+
+      def classes
+        self[:class]&.split || []
+      end
     end
   end
 end
