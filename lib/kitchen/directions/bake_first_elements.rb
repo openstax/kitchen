9c5d648923e43c8e0f3b057c15bc5ad2e3e77227
KarinaMendez2 <13000365@galileo.edu>
2021-05-24 17:42:11 +0000

    add tags

--- lib/kitchen/directions/bake_first_elements.rb
+++ lib/kitchen/directions/bake_first_elements.rb
@@ -5,10 +5,10 @@ module Kitchen
     module BakeFirstElements
       def self.v1(within:)
         selectors = [
-          '.os-problem-container > .os-table',
-          '.os-problem-container > [data-type="media"]',
-          '.os-solution-container > .os-table',
-          '.os-solution-container > [data-type="media"]'
+          'div.os-problem-container > div.os-table',
+          'div.os-problem-container > span[data-type="media"]',
+          'div.os-solution-container > div.os-table',
+          'div.os-solution-container > span[data-type="media"]'
         ]
         selectors.each do |selector|
           within.search("#{selector}:first-child").each do |problem|
