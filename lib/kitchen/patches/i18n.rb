fa6f80c8ddb1324574d9caec6e4ea6ff83083ed3
JP Slavinsky <jpslav@gmail.com>
2021-05-24 16:30:28 -0600

    add comment

--- lib/kitchen/patches/i18n.rb
+++ lib/kitchen/patches/i18n.rb
@@ -5,6 +5,7 @@ require 'twitter_cldr'
 # rubocop:disable Style/Documentation
 module I18n
   def self.sort_strings(first, second)
+    # TwitterCldr does not know about our :test locale, so substitute the English one
     locale = I18n.locale == :test ? :en : I18n.locale
     @sorters ||= {}
     sorter = @sorters[locale] ||= TwitterCldr::Collation::Collator.new(locale)
