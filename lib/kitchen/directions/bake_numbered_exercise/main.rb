c71b23e905ff0b1f80c519857115258052643409
ajluk <ajlukasik@gmail.com>
2021-05-26 12:07:28 +0000

    rework main file with spec

--- lib/kitchen/directions/bake_numbered_exercise/main.rb
+++ lib/kitchen/directions/bake_numbered_exercise/main.rb
@@ -3,17 +3,13 @@
 module Kitchen
   module Directions
     module BakeNumberedExercise
-      def self.v1(exercise:, number:, suppress_solution: false, note_suppressed_solutions: false)
-        V1.new.bake(exercise: exercise, number: number, suppress_solution: suppress_solution,
+      def self.v1(exercise:, number:, suppress_solution_if: false, note_suppressed_solutions: false)
+        V1.new.bake(exercise: exercise, number: number, suppress_solution_if: suppress_solution_if,
                     note_suppressed_solutions: note_suppressed_solutions)
       end
 
       def self.bake_solution_v1(exercise:, number:, divider: '. ')
         V1.new.bake_solution(exercise: exercise, number: number, divider: divider)
-      end
-
-      def self.bake_even_solution_v1(exercise:, divider: '. ')
-        V1.new.bake_even_solution(exercise: exercise, divider: divider)
       end
     end
   end
