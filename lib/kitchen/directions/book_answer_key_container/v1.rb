3ec8987dd700c7de9d77b1c3f2c408a74d2efa49
ajluk <ajlukasik@gmail.com>
2021-06-08 11:47:18 +0000

    remove klass solutions from BookAnswerKeyContainer.v1 and commented code

--- lib/kitchen/directions/book_answer_key_container/v1.rb
+++ lib/kitchen/directions/book_answer_key_container/v1.rb
@@ -3,7 +3,7 @@
 module Kitchen::Directions::BookAnswerKeyContainer
   class V1
     renderable
-    def bake(book:, klass: 'solutions')
+    def bake(book:, klass:)
       @klass = klass
       @metadata = book.metadata.children_to_keep.copy
       book.body.append(child: render(file: 'eob_answer_key_outer_container.xhtml.erb'))
