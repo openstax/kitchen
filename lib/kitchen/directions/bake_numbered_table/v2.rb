4ad5eb72c073d2f4f377c36153590c303fa81d96
Kendra Swanson <kendraswan@gmail.com>
2021-06-10 16:11:42 +0000

    v2 for numbered table captions

---
+++ lib/kitchen/directions/bake_numbered_table/v2.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Kitchen::Directions::BakeNumberedTable
+  # Difference from v1: only in the caption
+  class V2
+
+    def bake(table:, number:)
+      Kitchen::Directions::BakeNumberedTable::V1.new.bake_table(table: table, number: number)
+
+      caption = ''
+      if table&.caption&.first("span[data-type='title']")
+        caption_el = table.caption
+        caption_el.add_class('os-caption')
+        caption_el.name = 'span'
+        caption = caption_el.cut
+      end
+
+      table.append(sibling:
+        <<~HTML
+          <div class="os-caption-container">
+            <span class="os-title-label">#{I18n.t(:table_label)} </span>
+            <span class="os-number">#{number}</span>
+            <span class="os-divider"> </span>
+            #{caption}
+          </div>
+        HTML
+      )
+    end
+  end
+end
