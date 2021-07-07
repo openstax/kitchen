965af21e1915ff85195ff197fd8721edbc9e01ea
Kendra Swanson <kendraswan@gmail.com>
2021-07-07 17:08:00 +0000

    edit docs

--- lib/kitchen/directions/eoc_composite_page_container/main.rb
+++ lib/kitchen/directions/eoc_composite_page_container/main.rb
@@ -5,12 +5,13 @@ module Kitchen
     module EocCompositePageContainer
       # Creates a wrapper for the given content & appends it to the given element
       #
-      # @param container_key [String] both the lookup key for the container title & part of the wrapper class name, e.g. `'summary'`
+      # @param container_key [String] Appended to 'eoc.' to form the I18n key for the container title; also used as part of a class on the container.
       # @param uuid_key [String] the uuid key for the wrapper class, e.g. `'.summary'`
       # @param metadata_source [MetadataElement] metadata for the book
       # @param content [String] the content to be contained by the wrapper
-      # @param append_to [KitchenElement] the element to be appended, usually either a `ChapterElement` or a `CompositeChapterElement`
-      # @return [KitchenElement] the append_to element with container appended
+      # @param append_to [ElementBase] the element to be appended, usually either a `ChapterElement` or a `CompositeChapterElement`
+      # @return [ElementBase] the append_to element with container appended
+      #
       def self.v1(container_key:, uuid_key:, metadata_source:, content:,
                   append_to:)
         V1.new.bake(
--- lib/kitchen/directions/move_custom_section_to_eoc_container/main.rb
+++ lib/kitchen/directions/move_custom_section_to_eoc_container/main.rb
@@ -10,12 +10,13 @@ module Kitchen
       #
       # @param chapter [ChapterElement] the chapter in which the section to be moved is contained
       # @param metadata_source [MetadataElement] metadata for the book
-      # @param container_key [String] both the lookup key for the container title & part of the wrapper class name, e.g. `'summary'`
+      # @param container_key [String] Appended to 'eoc.' to form the I18n key for the container title; also used as part of a class on the container.
       # @param uuid_key [String] the uuid key for the wrapper class, e.g. `'.summary'`
       # @param section_selector [String] the selector for the section to be moved, e.g. `'section.summary'`
-      # @param append_to [KitchenElement] the element to be appended. Defaults to the value of `chapter` param if none given.
+      # @param append_to [ElementBase] the element to be appended. Defaults to the value of `chapter` param if none given.
       # @param include_intro_page [Boolean] control the introduction page for the chapter should be searched for a section to move, default is true
-      # @return [KitchenElement] the append_to element with container appended
+      # @return [ElementBase] the append_to element with container appended
+      #
       def self.v1(chapter:, metadata_source:, container_key:, uuid_key:,
                   section_selector:, append_to: nil, include_intro_page: true)
         V1.new.bake(
