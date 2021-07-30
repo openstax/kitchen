1c508f0796ab451ac639a738163e7e4a81883295
Mary Jo <mj81000@gmail.com>
2021-07-30 15:43:05 -0500

    Update eoc_section_title_link_snippet.rb

--- lib/kitchen/directions/eoc_section_title_link_snippet.rb
+++ lib/kitchen/directions/eoc_section_title_link_snippet.rb
@@ -28,6 +28,27 @@ module Kitchen
           </div>
         HTML
       end
+
+      def self.v3(page:)
+        if page.is_introduction?
+          os_number = ''
+        else
+          chapter = page.ancestor(:chapter)
+          os_number =
+            <<~HTML
+              <span class="os-number">#{chapter.count_in(:book)}.#{page.count_in(:chapter) - 1}</span>
+              <span class="os-divider"> </span>
+            HTML
+        end
+        <<~HTML
+          <a href="##{page.title.id}">
+            <h3 data-type="document-title" id="#{page.title.copied_id}">
+              #{os_number}
+              <span class="os-text" data-type="" itemprop="">#{page.title_text}</span>
+            </h3>
+          </a>
+        HTML
+      end
     end
   end
 end
