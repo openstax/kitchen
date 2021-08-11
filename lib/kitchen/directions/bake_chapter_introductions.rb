9c427bbe55bea2d83b058f87642a3a89a490cab2
Mary Jo <mj81000@gmail.com>
2021-08-11 16:33:00 -0500

    create a v2

--- lib/kitchen/directions/bake_chapter_introductions.rb
+++ lib/kitchen/directions/bake_chapter_introductions.rb
@@ -7,22 +7,7 @@ module Kitchen
         book.chapters.each do |chapter|
           chapter_outline_html = ''
 
-          case bake_chapter_objectives
-          when 'div'
-            chapter_objectives = chapter.first('div.chapter-objectives')&.cut
-            if chapter_objectives
-              chapter_objectives.search('div[data-type="title"]')&.trash
-              chapter_objectives.wrap_children('div', class: 'os-note-body')
-              chapter_objectives.prepend(child:
-                <<~HTML
-                  <h3 class="os-title" data-type="title">
-                    <span class="os-title-label">#{I18n.t(:chapter_objectives)}</span>
-                  </h3>
-                HTML
-              )
-              chapter_outline_html = chapter_objectives.cut
-            end
-          when true
+          if bake_chapter_objectives
             outline_items_html = chapter.non_introduction_pages.map do |page|
               <<~HTML
                 <div class="os-chapter-objective">
@@ -75,6 +60,75 @@ module Kitchen
       def self.v1_update_selectors(something_with_selectors)
         something_with_selectors.selectors.title_in_introduction_page =
           ".intro-text > [data-type='document-title']"
+      end
+
+      def self.v2(book:, chapter_objective_strategy: :default)
+        book.chapters.each do |chapter|
+          chapter_outline_html = ''
+
+          case chapter_objective_strategy
+          when :preexisting_title
+            chapter_objectives = chapter.first('div.chapter-objectives')&.cut
+            if chapter_objectives
+              chapter_objectives.search('div[data-type="title"]')&.trash
+              chapter_objectives.wrap_children('div', class: 'os-note-body')
+              chapter_objectives.prepend(child:
+                <<~HTML
+                  <h3 class="os-title" data-type="title">
+                    <span class="os-title-label">#{I18n.t(:chapter_objectives)}</span>
+                  </h3>
+                HTML
+              )
+              chapter_outline_html = chapter_objectives.cut
+            end
+          when :default
+            outline_items_html = chapter.non_introduction_pages.map do |page|
+              <<~HTML
+                <div class="os-chapter-objective">
+                  <a class="os-chapter-objective" href="##{page.title[:id]}">
+                    <span class="os-number">#{chapter.count_in(:book)}.#{page.count_in(:chapter)}</span>
+                    <span class="os-divider"> </span>
+                    <span data-type="" itemprop="" class="os-text">#{page.title.children[0].text}</span>
+                  </a>
+                </div>
+              HTML
+            end.join('')
+
+            chapter_outline_html = <<~HTML
+              <div class="os-chapter-outline">
+                <h3 class="os-title">#{I18n.t(:chapter_outline)}</h3>
+                #{outline_items_html}
+              </div>
+            HTML
+          end
+
+          introduction_page = chapter.introduction_page
+
+          introduction_page.search("div[data-type='description']").trash
+          introduction_page.search("div[data-type='abstract']").trash
+
+          title = introduction_page.title.cut
+          title.name = 'h2'
+          MoveTitleTextIntoSpan.v1(title: title)
+
+          intro_content = introduction_page
+                          .search("> :not([data-type='metadata']):not(.splash):not(.has-splash)")
+                          .cut
+
+          introduction_page.append(child:
+            <<~HTML
+              <div class="intro-body">
+                #{chapter_outline_html}
+                <div class="intro-text">
+                  #{title.paste}
+                  #{intro_content.paste}
+                </div>
+              </div>
+            HTML
+          )
+        end
+
+        v1_update_selectors(book)
       end
     end
   end
--- spec/directions/bake_chapter_introductions_spec.rb
+++ spec/directions/bake_chapter_introductions_spec.rb
@@ -205,9 +205,142 @@ RSpec.describe Kitchen::Directions::BakeChapterIntroductions do
     end
   end
 
-  context 'when v1 is called on a book with chapter-objectives: div' do
+  context 'when v2 called on book with default chapter objectives' do
     it 'works' do
-      described_class.v1(book: book_with_intro_objectives, bake_chapter_objectives: 'div')
+      described_class.v2(book: book)
+      expect(book.body).to match_normalized_html(
+        <<~HTML
+          <body>
+            <div data-type="chapter">
+              <h1 data-type="document-title">Chapter 1 Title</h1>
+              <div class="introduction" data-type="page">
+                <div data-type="metadata">don't touch this</div>
+                <figure class="splash">can't touch this (stop! hammer time)</figure>
+                <div class="intro-body">
+                  <div class="os-chapter-outline">
+                    <h3 class="os-title">Chapter Outline</h3>
+                    <div class="os-chapter-objective">
+                      <a class="os-chapter-objective" href="#becomes-ref-link">
+                        <span class="os-number">1.1</span>
+                        <span class="os-divider"> </span>
+                        <span class="os-text" data-type="" itemprop="">should be objective 1.1</span>
+                      </a>
+                    </div>
+                    <div class="os-chapter-objective">
+                      <a class="os-chapter-objective" href="#">
+                        <span class="os-number">1.2</span>
+                        <span class="os-divider"> </span>
+                        <span class="os-text" data-type="" itemprop="">should be objective 1.2</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="intro-text">
+                    <h2 data-type="document-title">
+                      <span class="os-text" data-type="" itemprop="">Introduction 1</span>
+                    </h2>
+                    <figure>move this</figure>
+                    <div>content</div>
+                  </div>
+                </div>
+              </div>
+              <div data-type="page">
+                <div data-type="document-title" id="becomes-ref-link">should be objective 1.1</div>
+              </div>
+              <div data-type="page">
+                <div data-type="document-title">should be objective 1.2</div>
+              </div>
+            </div>
+            <div data-type="chapter">
+              <h1 data-type="document-title">Chapter 2 Title</h1>
+              <div class="introduction" data-type="page">
+                <div class="intro-body">
+                  <div class="os-chapter-outline">
+                    <h3 class="os-title">Chapter Outline</h3>
+                    <div class="os-chapter-objective">
+                      <a class="os-chapter-objective" href="#">
+                        <span class="os-number">2.1</span>
+                        <span class="os-divider"> </span>
+                        <span class="os-text" data-type="" itemprop="">should be objective 2.1</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="intro-text">
+                    <h2 data-type="document-title">
+                      <span class="os-text" data-type="" itemprop="">Introduction 2</span>
+                    </h2>
+                    <div>content</div>
+                  </div>
+                </div>
+              </div>
+              <div data-type="page">
+                <div data-type="document-title">should be objective 2.1</div>
+              </div>
+            </div>
+          </body>
+        HTML
+      )
+    end
+
+    it 'updates selectors' do
+      expect { described_class.v2(book: book) }.to change {
+        book.selectors.title_in_introduction_page
+      }.from("./*[@data-type = 'document-title']").to(".intro-text > [data-type='document-title']")
+    end
+  end
+
+  context 'when v2 called on book without chapter objectives' do
+    it 'works' do
+      described_class.v2(book: book, chapter_objective_strategy: nil)
+      expect(book.body).to match_normalized_html(
+        <<~HTML
+          <body>
+            <div data-type="chapter">
+              <h1 data-type="document-title">Chapter 1 Title</h1>
+              <div class="introduction" data-type="page">
+                <div data-type="metadata">don't touch this</div>
+                <figure class="splash">can't touch this (stop! hammer time)</figure>
+                <div class="intro-body">
+                  <div class="intro-text">
+                    <h2 data-type="document-title">
+                      <span class="os-text" data-type="" itemprop="">Introduction 1</span>
+                    </h2>
+                    <figure>move this</figure>
+                    <div>content</div>
+                  </div>
+                </div>
+              </div>
+              <div data-type="page">
+                <div data-type="document-title" id="becomes-ref-link">should be objective 1.1</div>
+              </div>
+              <div data-type="page">
+                <div data-type="document-title">should be objective 1.2</div>
+              </div>
+            </div>
+            <div data-type="chapter">
+              <h1 data-type="document-title">Chapter 2 Title</h1>
+              <div class="introduction" data-type="page">
+                <div class="intro-body">
+                  <div class="intro-text">
+                    <h2 data-type="document-title">
+                      <span class="os-text" data-type="" itemprop="">Introduction 2</span>
+                    </h2>
+                    <div>content</div>
+                  </div>
+                </div>
+              </div>
+              <div data-type="page">
+                <div data-type="document-title">should be objective 2.1</div>
+              </div>
+            </div>
+          </body>
+        HTML
+      )
+    end
+  end
+
+  context 'when v2 is called on a book with chapter-objectives: preexisting-title' do
+    it 'works' do
+      described_class.v2(book: book_with_intro_objectives, chapter_objective_strategy: :preexisting_title)
       expect(book_with_intro_objectives.body).to match_normalized_html(
         <<~HTML
           <body>
