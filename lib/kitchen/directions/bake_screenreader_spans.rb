209e52182e0e6199d1f275740d223bb3fe404988
Kendra Swanson <kendraswan@gmail.com>
2021-11-08 18:01:14 +0000

    linter fix

--- lib/kitchen/directions/bake_screenreader_spans.rb
+++ lib/kitchen/directions/bake_screenreader_spans.rb
@@ -7,7 +7,9 @@ module Kitchen
       # Additional screenreader spans can be added below.
       def self.v1(book:)
         book.search('u[data-effect="double-underline"]').each do |element|
-          element.add_previous_sibling('<span data-screenreader-only="true">double underline</span>')
+          element.add_previous_sibling(
+            '<span data-screenreader-only="true">double underline</span>'
+          )
         end
       end
     end
