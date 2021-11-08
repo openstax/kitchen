cf294676046a7f3a1c103bc28e4906fe928e59e4
Kendra Swanson <kendraswan@gmail.com>
2021-11-08 17:24:57 +0000

    add BakeScreenreaderSpans direction

---
+++ lib/kitchen/directions/bake_screenreader_spans.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Kitchen
+  module Directions
+    module BakeScreenreaderSpans
+      # Add text for accessibility.
+      # Additional screenreader spans can be added below.
+      def self.v1(book:)
+        book.search('u[data-effect="double-underline"]').each do |element|
+          element.add_previous_sibling('<span data-screenreader-only="true">double underline</span>')
+        end
+      end
+    end
+  end
+end
